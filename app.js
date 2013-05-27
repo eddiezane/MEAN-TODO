de9c596b2d5cd1b4713e8a9793af1957c8c27735
Eddie Zaneski <eddiezane@gmail.com>
2013-05-26 23:52:29 -0400

    Server assets and add npm package

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-var app = require('express')();
+var app = require('express')()
+  , port = 3000;
+
+if (process.env.NODE_ENV == 'production')
+  port = 80;
+
+app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/views/index.html');
